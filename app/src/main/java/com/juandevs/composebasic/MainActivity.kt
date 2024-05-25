5877a31869bfa30e27b05a100274c9c8056b5ff4
Juanchodevs <juan0acu@gmail.com>
2024-05-25 12:47:51 -0400

    Sección 4: TextFlied y Sección 5

--- app/src/main/java/com/juandevs/composebasic/MainActivity.kt
+++ app/src/main/java/com/juandevs/composebasic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.juandevs.composebasic
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,27 +13,43 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.juandevs.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeBasicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Exercise(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    var myText by remember { mutableStateOf("") }
+
+                    Column {
+                        MyTextFliedOutlined(myText) { myText = it }
+                    }
+
                 }
             }
         }
@@ -88,10 +105,78 @@ fun Exercise(name: String, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun MyTextFliedOutlined(name: String, onValueChange: (String) -> Unit) {
+
+    OutlinedTextField(
+        value = name,
+        onValueChange = { onValueChange(it) },
+        modifier = Modifier.padding(24.dp),
+        label = {
+            Text(
+                text = "Example name"
+            )
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = Color.Green,
+            unfocusedBorderColor = Color.Red
+        )
+    )
+}
+
+@Composable
+fun MyTextAdvance() {
+    var myText by remember { mutableStateOf("") }
+    TextField(
+        value = myText,
+        onValueChange = {
+            myText = if (it.contains("a")) {
+                it.replace("a", "")
+            } else {
+                it
+            }
+        },
+        label = { Text(text = "Introduce tu nombre") })
+}
+
+@Composable
+fun MyTextField() {
+    var myText by remember { mutableStateOf("") }
+    TextField(value = myText, onValueChange = { myText = it })
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
+        Text(
+            text = "Esto es un ejemplo",
+            textDecoration = TextDecoration.LineThrough
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            textDecoration = TextDecoration.Underline
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            textDecoration = TextDecoration.combine(
+                listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+            )
+        )
+        Text(
+            text = "Esto es un ejemplo", fontSize = 22.sp
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeBasicTheme {
-        Exercise("Android")
+        MyTextAdvance()
     }
 }
